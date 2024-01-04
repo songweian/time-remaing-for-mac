4d1b78890a188140a291fbda8f28ca58649be095
weian404 <weian404@gmail.com>
2024-01-04 20:23:08 +0800

    Initial Commit

---
+++ TimeRemaining/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TimeRemaining
+//
+//  Created by swa-c on 2024/1/4.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
